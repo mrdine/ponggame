4edf568275d921ca7de8060742efc87531e0c4c1
mrdine <daniel.gomes071@gmail.com>
2021-08-10 13:59:27 -0300

    first commit

---
+++ src/public/game/game.js
@@ -0,0 +1,18 @@
+
+const config = {
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  audio: {
+    disableWebAudio: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      fps: 60,
+      gravity: {y : 0},
+    }
+  },
+};
+
+const game = new Phaser.Game(config);
